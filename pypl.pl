0c54dddcaac34a56b48f392f3aa667f1ca98e3c7
z5129023 <z5129023@cse.unsw.edu.au>
2017-09-15 00:25:01 +1000

    Zhihao Ye's code

--- pypl.pl
+++ pypl.pl
@@ -1,33 +1,31 @@
 #!/usr/bin/perl -w
+while($line=<>){
+  # newline
+  if ($line!~ /\n$/){
+      $line="$line\n";
+  }
+  # interpreter
+  if ($line=~ /^#!\/.*/){
+    $line=~ s/.*/#!\/usr\/bin\/perl -w/;
+  }
+  # print strings
+  if ($line=~ /print\(\".*\"\)/){
+    $line=~ s/print\(/print /;
+    $line=~ s/\"\)/\\n\";/;
+  }
+  # print variables
+  if ($line=~ /print\([a-zA-Z_][a-zA-Z0-9_]*\)/){
+    $line=~ s/print\(/print \"\$/;
+    $line=~ s/\)/\\n\";/;
+  }
+  # numeric constants and operators
+  if ($line=~ /^([a-zA-Z_][a-zA-Z0-9_]*)([ ]*=[ ]*(\d+)([ ]*[\+|\-|\*|\/|\/\/|%|\*\*][ ]*\d+)*)/){
+    $v_name=$1;
+    $right_side=$2;
+    $line="\$$v_name$right_side;\n";
+  }
 
-# Starting point for COMP[29]041 assignment 1
-# http://www.cse.unsw.edu.au/~cs2041/assignments/pypl
-# written by andrewt@unsw.edu.au September 2017
 
-while ($line = <>) {
-    if ($line =~ /^#!/ && $. == 1) {
 
-        # translate #! line
-
-        print "#!/usr/bin/perl -w\n";
-
-    } elsif ($line =~ /^\s*(#|$)/) {
-
-        # Blank & comment lines can be passed unchanged
-
-        print $line;
-
-    } elsif ($line =~ /^\s*print\("(.*)"\)$/) {
-
-        # Python's print outputs a new-line character by default
-        # so we need to add it explicitly to the Perl print statement
-
-        print "print \"$1\\n\";\n";
-
-    } else {
-
-        # Lines we can't translate are turned into comments
-
-        print "#$line\n";
-    }
+  print "$line";
 }
