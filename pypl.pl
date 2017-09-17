c98186552eda2893d51d1c3df16c9bb5d2d0e762
z5129023 <z5129023@cse.unsw.edu.au>
2017-09-18 02:08:18 +1000

    fix some bugs and finish the subset1

--- pypl.pl
+++ pypl.pl
@@ -14,17 +14,28 @@ while($line=<>){
     $line=~ s/\"\)/\\n\";/;
   }
   # print variables
-  if ($line=~ /print\([a-zA-Z_][a-zA-Z0-9_]*\)/){
+  elsif ($line=~ /print\([a-zA-Z_][a-zA-Z0-9_]*\)/){
     $line=~ s/print\(/print \"\$/;
     $line=~ s/\)/\\n\";/;
   }
+  # print operators among variables
+  elsif ($line=~ /print\([ ]*(?:[a-zA-Z_][a-zA-Z0-9_]*)(?:[ ]*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))[ ]*(?:[a-zA-Z_][a-zA-Z0-9_]*))+[ ]*\)/){
+    $line=~ s/([a-zA-Z_][a-zA-Z0-9_]*)/\$$1/g;
+    $line=~ s/\$print\(/print /;
+    $line=~ s/\)/, \"\\n\";/;
+  }
   # numeric constants and operators
-  if ($line=~ /^([a-zA-Z_][a-zA-Z0-9_]*)([ ]*=[ ]*(\d+)([ ]*[\+|\-|\*|\/|\/\/|%|\*\*][ ]*\d+)*)/){
+  if ($line=~ /^([a-zA-Z_][a-zA-Z0-9_]*)([ ]*=[ ]*(?:\d+)(?:[ ]*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))[ ]*\d+)*)/){
     $v_name=$1;
     $right_side=$2;
     $line="\$$v_name$right_side;\n";
   }
-
+  # operators among variables
+  if ($line=~ /^[a-zA-Z_][a-zA-Z0-9_]*[ ]*=[ ]*(?:[a-zA-Z_][a-zA-Z0-9_]*)(?:[ ]*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))[ ]*(?:[a-zA-Z_][a-zA-Z0-9_]*))*/){
+    $line=~ s/([a-zA-Z_][a-zA-Z0-9_]*)/\$$1/g;
+    chomp $line;
+    $line="$line;\n";
+  }
 
 
   print "$line";
