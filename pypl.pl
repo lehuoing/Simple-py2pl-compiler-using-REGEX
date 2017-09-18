514e7d10979ed134008916a283a11af3d49925eb
z5129023 <z5129023@cse.unsw.edu.au>
2017-09-19 00:08:24 +1000

    fix some bugs

--- pypl.pl
+++ pypl.pl
@@ -14,27 +14,26 @@ while($line=<>){
     $line=~ s/\"\)/\\n\";/;
   }
   # print variables
-  elsif ($line=~ /print\([a-zA-Z_][a-zA-Z0-9_]*\)/){
+  elsif ($line=~ /print\(\s*[a-zA-Z_][a-zA-Z0-9_]*\s*\)/){
     $line=~ s/print\(/print \"\$/;
     $line=~ s/\)/\\n\";/;
   }
   # print operators among variables
-  elsif ($line=~ /print\([ ]*(?:[a-zA-Z_][a-zA-Z0-9_]*)(?:[ ]*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))[ ]*(?:[a-zA-Z_][a-zA-Z0-9_]*))+[ ]*\)/){
+  elsif ($line=~ /print\(\s*(?:[a-zA-Z_][a-zA-Z0-9_]*)(?:\s*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))\s*(?:[a-zA-Z_][a-zA-Z0-9_]*))+\s*\)/){
     $line=~ s/([a-zA-Z_][a-zA-Z0-9_]*)/\$$1/g;
     $line=~ s/\$print\(/print /;
     $line=~ s/\)/, \"\\n\";/;
   }
   # numeric constants and operators
-  if ($line=~ /^([a-zA-Z_][a-zA-Z0-9_]*)([ ]*=[ ]*(?:\d+)(?:[ ]*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))[ ]*\d+)*)/){
+  if ($line=~ /^([a-zA-Z_][a-zA-Z0-9_]*)(\s*=\s*(?:\d+)(?:\s*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))\s*\d+)*)/){
     $v_name=$1;
     $right_side=$2;
     $line="\$$v_name$right_side;\n";
   }
   # operators among variables
-  if ($line=~ /^[a-zA-Z_][a-zA-Z0-9_]*[ ]*=[ ]*(?:[a-zA-Z_][a-zA-Z0-9_]*)(?:[ ]*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))[ ]*(?:[a-zA-Z_][a-zA-Z0-9_]*))*/){
+  if ($line=~ /^[a-zA-Z_][a-zA-Z0-9_]*\s*=\s*(?:[a-zA-Z_][a-zA-Z0-9_]*)(?:\s*(?:[\+\-\*\/%]|(?:\/\/)|(?:\*\*))\s*(?:[a-zA-Z_][a-zA-Z0-9_]*))*/){
     $line=~ s/([a-zA-Z_][a-zA-Z0-9_]*)/\$$1/g;
-    chomp $line;
-    $line="$line;\n";
+    $line=~ s/\n/;\n/;
   }
 
 
